897d593a343af629174a7efa6a75a81480a8a30e
teojunjie <teojunjie@gmail.com>
2019-10-19 11:41:18 -0700

    Added list elements endpoint

--- elements/urls.py
+++ elements/urls.py
@@ -1,11 +1,24 @@
 from django.conf.urls import url
-from .views import PopulateElements
+from .views import (
+    PopulateElements,
+    ListPeriodicElements,
+    GetPeriodicElement,
+)
 
 EXTERNAL_URL_PREFIX = 'external/v1'
 
 urlpatterns = [
     url(r'^{url_prefix}/elements/populate?$'.format(
         url_prefix=EXTERNAL_URL_PREFIX,
-    ), PopulateElements.as_view(), name='external_populate_list'),
+    ), PopulateElements.as_view(), name='external_populate_elements'),
+
+    url(r'^{url_prefix}/elements/list?$'.format(
+        url_prefix=EXTERNAL_URL_PREFIX,
+    ), ListPeriodicElements.as_view(), name='external_list_elements'),
+
+    url(r'^{url_prefix}/elements/(?P<element_name>[^/]+)/get?$'.format(
+        url_prefix=EXTERNAL_URL_PREFIX,
+    ), GetPeriodicElement.as_view(), name='external_list_elements'),
+
 
 ]
